19c60a883a01c4325000d784e05e4101d3b22dad
Simey de Klerk <simeydeklerk@outlook.com>
2018-10-23 19:38:43 +0200

    refactor show to stateless functinoal component

--- components/Show.js
+++ components/Show.js
@@ -5,6 +5,7 @@ import Router from 'next/router';
 import { FaPlay } from 'react-icons/fa';
 import Bars from './bars';
 
+<<<<<<< HEAD
 export default class Show extends React.Component {
   static propTypes = {
     show: PropTypes.object.isRequired,
@@ -55,3 +56,32 @@ export default class Show extends React.Component {
     );
   }
 }
+=======
+const Show = ({ show, currentPlaying, currentShow, setCurrentPlaying }) => {
+    
+  const [isPlaying, isDisplayed] = [currentPlaying, currentShow].map(currentX => show.displayNumber === currentX)
+  const showClassName = ['show', isPlaying ? 'show--playing' : '', isDisplayed ? 'show--active' : ''].join(' ')
+
+  const changeURL = (e, show) => {
+    e.preventDefault();
+    const href = e.currentTarget.href;
+    Router.push(`/?number=${show.displayNumber}`, href, { shallow: true })
+  }
+  const PlayButton = () => <button onClick={() => setCurrentPlaying(show.displayNumber)} className="show__play" title="play button" ><FaPlay/></button>
+
+  return (
+    <div className={showClassName}>
+      <a className="show__link" href={`/show/${show.displayNumber}/${slug(show.title)}`} onClick={(e) => changeURL(e, show)}>
+        <p className="show__displayNumber">Episode {show.displayNumber}</p>
+        <h3 className="show__title">{show.title}</h3>
+      </a>
+
+      <div className="show__playcontrols">
+        {isPlaying ? <Bars/ > :  <PlayButton />}
+      </div>
+    </div>
+  )
+};
+
+export default Show;
+>>>>>>> 965a4f58707367431b1858cc16d6bab8545d5a52
